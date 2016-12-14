af306384de2bbbf409ac4a1c45b8ec140a5574a9
Mike Axtman <mdaxtman@gmail.com>
2016-12-13 22:31:57 -0800

    finish production build"

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,42 +1,57 @@
-const WebpackBrowserPlugin = require("webpack-browser-plugin");
-const BellOnBundlerErrorPlugin = require("bell-on-bundler-error-plugin");
+var path = require("path");
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// module.exports = {
-//   context: __dirname + "/src",
-// 	entry: {
-//     app: "./app.js",
-//   },
-//   output: {
-//     path: __dirname + "/dist",
-//     filename: "[name].bundle.js",
-//     publicPath: "/assets"
-//   },
-//   devServer: {
-//     contentBase: __dirname + "/src",
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         use: [{
-//           loader: "babel-loader",
-//         }],
-//         exclude: /node_modules/,
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [
-//           "style-loader",
-//           {
-//             loader: "css-loader",
-//             options: {
-//               modules: true,
-//               sourceMap: true,
-//             },
-//           },
-//         ],
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-// };
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public/"),
+    filename: "app.bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loaders: ExtractTextPlugin.extract([
+         "css-loader?modules&localIdentName=[hash:base64:10]&importLoaders=1",
+         "postcss-loader",
+        ]),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json$/,
+        loaders: ["json-loader"], 
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|png|jpg|gif)$/,
+        loaders: ["url-loader?name=assets/[hash:12].[ext]&limit=10000"], 
+        exclude: /node_modules/,
+      },
+      {
+        test: /.svg$/,
+        loaders: [
+          "raw-loader",
+          "image-webpack-loader",
+        ],
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin("styles.css"),
+    new HtmlWebpackPlugin({
+      template: "index.template.html",
+      inject: "body"
+    })
+  ],
+}
