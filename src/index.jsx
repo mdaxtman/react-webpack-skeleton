a77b7e9caa4562b8db65bff4ff03483ca6b8f083
Mike Axtman <mdaxtman@gmail.com>
2017-12-25 15:35:48 -0800

    update all outdated dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import "./global.css";
 
-ReactDOM.render(/* insert app here */, document.getElementById("root"));
+ReactDOM.render(<div>hi\</div>, document.getElementById("root"));
 
 if (module.hot) {
   module.hot.accept();
