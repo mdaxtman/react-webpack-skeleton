986fbae19875d54140ec7ddbcba4d7dbad18b3c6
Mike Axtman <mdaxtman@gmail.com>
2017-12-25 16:02:39 -0800

    remove un-needed jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import "./global.css";
 
-ReactDOM.render(<div>hi\</div>, document.getElementById("root"));
+ReactDOM.render(/*insert jsx here*/, document.getElementById("root"));
 
 if (module.hot) {
   module.hot.accept();
